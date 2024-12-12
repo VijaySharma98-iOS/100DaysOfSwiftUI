a81d40040fbd74d5b9f004deed04f1cf18d124a9
Vijay Sharma <113002348+VijaySharma898@users.noreply.github.com>
2024-12-12 15:43:26 +0530

    First Commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vijay Sharma on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
