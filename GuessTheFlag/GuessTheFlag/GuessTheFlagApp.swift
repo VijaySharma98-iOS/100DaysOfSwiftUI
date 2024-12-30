ab8914afb5b417d413976b90f2ba020bdf2891c7
Vijay Sharma <113002348+VijaySharma898@users.noreply.github.com>
2024-12-30 21:12:22 +0530

    Complete the guess the Flag UI

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Vijay Sharma on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
